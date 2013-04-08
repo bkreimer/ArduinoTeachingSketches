4e3f3cac09f7150aaf10201d9c566ff719b1897f
Jeff Thompson <mail@jeffreythompson.org>
2013-04-08 12:08:31 -0500

    Fixed some Tone library examples

--- Sound/UsingTheToneLibrary/UsingTheToneLibrary.ino
+++ Sound/UsingTheToneLibrary/UsingTheToneLibrary.ino
@@ -2,22 +2,24 @@
 USING THE TONE LIBRARY
  Jeff Thompson | 2013 | www.jeffreythompson.org
  
- A fancier Tone library is also available; download and details here:
- http://code.google.com/p/rogue-code/wiki/ToneLibraryDocumentation
+ Simple tone output (as of Arduino 1.0) is now built in
+ and requires no external libraries. You are limited to
+ one tone at a time - for a fancier version, see the "Chords"
+ sketch in this section.
  
- You may have to add the following line to the Tone.cpp file in the
- library to get it to work with Arduino 1.0:
-   #include <Arduino.h>
-   
+ To more easily create note values, we also import a file called
+ "pitches.h" so we can ask for the notes by name. 
  */
 
+#include "pitches.h"          // import a file called pitches.h (see tab for details)
 
 void setup() {
   // nothing here!
 }
 
 void loop() {
-  if (millis() % 500 == 0) {
-    tone(13, NOTE_C4, 250);
+  if (millis() % 500 == 0) {  // make a note every 500ms
+    tone(7, NOTE_C4, 250);    // on pin 7, generate C4 for 250 ms
   }
 }
+
